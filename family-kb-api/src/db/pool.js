3a871ada285a2b5dde7e2f4fe0270ecd109f2807
Stone Yuan <jackyuan110@gmail.com>
2026-05-16 21:02:01 +0000

    docker config debug

--- family-kb-api/src/db/pool.js
+++ family-kb-api/src/db/pool.js
@@ -2,6 +2,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DATABASE_URL:', process.env.DATABASE_URL); // add this
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 })
