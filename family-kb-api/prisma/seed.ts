2694ecbba8b1624ff8d0b538b4000cb4f6c656e5
stoneYuan3 <jackyuan110@gmail.com>
2026-05-16 11:21:58 -0700

    fixing prisma seeding issue

--- family-kb-api/prisma/seed.ts
+++ family-kb-api/prisma/seed.ts
@@ -1,27 +1,38 @@
-import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/client";
 import bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  const passwordHash = await bcrypt.hash('Stone001219@', 10);
+  const passwordHash = await bcrypt.hash('admin260516', 10);
 
-  await prisma.user.upsert({
+  const admin = await prisma.users.upsert({
     where: { email: 'jackyuan110@gmail.com' },
     update: {},
     create: {
       name: 'shucong',
       email: 'jackyuan110@gmail.com',
       password: passwordHash,
-      role: 'ADMIN',
+      role: 'admin',
     },
   });
+  console.log({admin})
 }
 
 main()
-  .then(() => prisma.$disconnect())
+  .then(async () => {
+    await prisma.$disconnect();
+    await pool.end();
+  })
   .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
+    await pool.end();
     process.exit(1);
   });
